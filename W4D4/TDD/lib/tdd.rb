1c941f1ac3a23ca023f56f7a2209194f68ee20b0
skumtong <sky_kumtong@yahoo.com>
2021-02-11 15:29:44 -0800

    Create stock_picker

--- W4D4/TDD/lib/tdd.rb
+++ W4D4/TDD/lib/tdd.rb
@@ -6,6 +6,7 @@ class Array
   def two_sum
     arr = self
     result = []
+
     (0...arr.length - 1).each do |i|
       (i+1...arr.length).each do |j|
         result << [i, j] if arr[i] + arr[j] == 0
@@ -15,3 +16,25 @@ class Array
     result
   end
 end
+
+def my_transpose(arr)
+  arr.transpose 
+end
+
+def stock_picker(arr) 
+    most_profit = 0
+    result = []
+
+    (0...arr.length - 1).each do |i|
+      (i+1...arr.length).each do |j|
+        profit =  arr[j] - arr[i]
+          if profit > most_profit
+            most_profit = profit            
+            result = [i, j] 
+         end
+      end
+    end
+
+    " Buy on day #{result[0]+1} and sell on day #{result[1]+1}"
+end
+
--- W4D4/TDD/spec/tdd_spec.rb
+++ W4D4/TDD/spec/tdd_spec.rb
@@ -1,7 +1,8 @@
 require "tdd.rb"
 
 describe "my_uniq" do
-   it "Removes duplicates in array" do
+
+  it "Removes duplicates in array" do
     expect(my_uniq([1,2,3,3])).to eq([1,2,3])
   end
 
@@ -13,15 +14,16 @@ describe "my_uniq" do
     expect(my_uniq([])).to eq([])
   end
 
-  # it "should not use the built-in uniq method" do
-  #    arr = [11, 5, 13, 2, 3]
-  #   expect_any_instance_of(Array).to_not receive(:uniq)
-  #    my_uniq(arr)
-  # end 
+  it "should not use the built-in uniq method" do
+    arr = [11, 5, 13, 2, 3]
+    expect_any_instance_of(Array).to_not receive(:uniq)
+    my_uniq(arr)
+  end   
 
 end
 
 describe "Array#two_sum" do 
+
   it "returns a 2-D array" do 
     array = [-1, 1, -2, 2]
     expect(array.two_sum.flatten).to_not eq(array.two_sum)
@@ -42,4 +44,39 @@ describe "Array#two_sum" do
     expect(array.two_sum).to_not eq([[0, 0]])
   end
 
+end
+
+describe "#my_transpose" do 
+
+  it "transposes correctly" do 
+    rows = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+  ]
+
+  expect(my_transpose(rows)).to eq([[0, 3, 6], [1, 4, 7], [2, 5, 8]])
+  end
+
+  it "should transpose with arrays of varying lengths" do
+    rows = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7]
+    ]
+
+    expect(my_transpose(rows)).to eq([[0, 3, 6], [1, 4, 7], [2, 5, nil]])
+  end
+
+  it "Should return an empty array if the input array is empty" do
+    expect(my_transpose([])).to eq([])
+  end
+end
+
+describe "#stock_picker" do 
+  it "should return the first case of the most profitable day" do 
+    array = [3,2,5,6,1,3,1]
+
+    expect(stock_picker(array)).to eq(" Buy on day #{2} and sell on day #{4}")
+  end
 end
