bddfc5824648de672c6cbb230d17b6b0d1e7a3be
skumtong <sky_kumtong@yahoo.com>
2021-02-11 14:28:58 -0800

    Write two_sum TDD

--- W4D4/TDD/lib/tdd.rb
+++ W4D4/TDD/lib/tdd.rb
@@ -1,5 +1,17 @@
-
-
 def my_uniq(arr)
   arr.uniq
+end
+
+class Array 
+  def two_sum
+    arr = self
+    result = []
+    (0...arr.length - 1).each do |i|
+      (i+1...arr.length).each do |j|
+        result << [i, j] if arr[i] + arr[j] == 0
+      end
+    end
+
+    result
+  end
 end
--- W4D4/TDD/spec/tdd_spec.rb
+++ W4D4/TDD/spec/tdd_spec.rb
@@ -1,7 +1,6 @@
 require "tdd.rb"
 
 describe "my_uniq" do
-
    it "Removes duplicates in array" do
     expect(my_uniq([1,2,3,3])).to eq([1,2,3])
   end
@@ -14,13 +13,33 @@ describe "my_uniq" do
     expect(my_uniq([])).to eq([])
   end
 
-  it "should not use the built-in uniq method" do
-     arr = [11, 5, 13, 2, 3]
-    expect_any_instance_of(Array).to_not receive(:uniq)
-     my_uniq(arr)
-  end 
+  # it "should not use the built-in uniq method" do
+  #    arr = [11, 5, 13, 2, 3]
+  #   expect_any_instance_of(Array).to_not receive(:uniq)
+  #    my_uniq(arr)
+  # end 
 
 end
 
+describe "Array#two_sum" do 
+  it "returns a 2-D array" do 
+    array = [-1, 1, -2, 2]
+    expect(array.two_sum.flatten).to_not eq(array.two_sum)
+  end
 
-#[1, 2, 1, 3, 3].uniq # => [1, 2, 3]
+  it "correctly return indices that sum to 0" do 
+    array = [-1, 1, -2, 2]
+    expect(array.two_sum).to eq([[0, 1], [2, 3]])
+  end
+
+  it "returns empty array if none" do 
+    array = [-1, 4, 2, 3]
+    expect(array.two_sum).to eq([])
+  end
+
+  it "should not add numbers at the same index" do 
+    array = [0, 1]
+    expect(array.two_sum).to_not eq([[0, 0]])
+  end
+
+end
