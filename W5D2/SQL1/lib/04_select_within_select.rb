28498a9f5d352b2abe99be7853472be545958d99
Wenchong Lai <wenchonglai@gmail.com>
2021-02-16 15:50:07 -0800

    Finish Phases 3-4 for W5D1 SQL Zoo Classwork

--- W5D2/SQL1/lib/04_select_within_select.rb
+++ W5D2/SQL1/lib/04_select_within_select.rb
@@ -35,6 +35,19 @@ end
 def larger_than_russia
   # List each country name where the population is larger than 'Russia'.
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      countries
+    WHERE
+      population > (
+        SELECT
+          population
+        FROM
+          countries
+        WHERE
+          name = 'Russia'
+      )
   SQL
 end
 
@@ -42,6 +55,19 @@ def richer_than_england
   # Show the countries in Europe with a per capita GDP greater than
   # 'United Kingdom'.
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      countries
+    WHERE
+      continent LIKE 'Eu%' AND
+      gdp / population > ( SELECT
+                            gdp / population AS gdp_per_cap
+                          FROM
+                            countries
+                          WHERE
+                            name = 'United Kingdom'
+                        )
   SQL
 end
 
@@ -49,6 +75,19 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+    SELECT
+      name, continent
+    FROM
+      countries
+    WHERE
+      continent IN (  SELECT
+                        continent
+                      FROM
+                        countries
+                      WHERE
+                        name IN ('Belize', 'Belgium')
+                    ) 
+
   SQL
 end
 
@@ -56,14 +95,47 @@ def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+    SELECT
+      name, population
+    FROM
+      countries
+    WHERE
+      population >  ( SELECT
+                        population
+                      FROM
+                        countries
+                      WHERE
+                        name LIKE 'Canada'
+                    ) AND
+      population <  ( SELECT
+                        population
+                      FROM
+                        countries
+                      WHERE
+                        name LIKE 'Poland'
+                    )
   SQL
 end
 
 def sparse_continents
-  # Find every country that belongs to a continent where each country's
-  # population is less than 25,000,000. Show name, continent and
+  # Find every country that does not belong to a continent where any of
+  # its countries
+  # have a population ≥ 25,000,000. Show name, continent and
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+    SELECT
+      name, continent, population
+    FROM
+      countries
+    WHERE
+      continent NOT IN (
+        SELECT
+          continent
+        FROM
+          countries
+        WHERE
+          population >= 25000000
+      )
   SQL
 end
