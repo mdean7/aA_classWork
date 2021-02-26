83bbdd52d9d4fe6a9034d8f4e3153bc9402b2a81
terryt925 <thindterry@yahoo.com>
2021-02-25 16:59:49 -0800

    Create artwork shares

--- W6D3/routes_and_controllers/app/models/artwork.rb
+++ W6D3/routes_and_controllers/app/models/artwork.rb
@@ -1,2 +1,20 @@
 class Artwork < ApplicationRecord
+
+validates :title, {scope: :artist_id}, uniqueness:true
+validates :image_url, uniqueness:true, presence:true
+validates :artist_id, uniqueness:true, presence:true
+
+
+belong_to :artist,
+  foreign_key: :artist_id,
+  class_name: :User
+
+ has_many :shares,
+  foreign_key: :artwork_id,
+  class_name: :ArtworkShare
+
+has_many :shared_viewers,
+  through: :shares,
+  source: :viewer
+
 end
